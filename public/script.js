f86da3c0261e870c3cd93a7ac708b22b451d5c29
Vitaliy Semenov <synthex@bk.ru>
2023-04-30 13:31:03 +0600

    add redux

--- public/script.js
+++ public/script.js
@@ -117,32 +117,6 @@ const changeLotPrice = (id, price) => ({
 
 // ###########################
 
-class Store {
-  constructor(reducer, initialState) {
-    this.reducer = reducer,
-    this.state = reducer(initialState, {type: null });
-    this.listeners = [];
-  }
-
-  getState() {
-    return this.state;
-  }
-
-  subscribe(listener) {
-    this.listeners.push(listener);
-
-    return () => {
-      const index = this.listeners.indexOf(listener);
-      this.listeners.splice(index, 1);
-    }
-  }
-
-  dispatch(action) {
-    this.state =  this.reducer(this.state, action);
-    this.listeners.forEach((listener) => listener());
-  }
-
-}
 
 function App({ state }) {
   return (
@@ -211,19 +185,7 @@ function renderView(state) {
   );
 }
 
-
-const combineReducers = (reducers) => {
-  return (state = {}, action) => {
-    const result = {};
-    Object.entries(reducers).forEach(([key, reducer]) => {
-      result[key] = reducer(state[key], action);
-    });
-
-    return result;
-  }
-}
-
-const store = new Store(combineReducers({
+const store = new Redux.createStore(Redux.combineReducers({
   clock: clockReducer,
   auction: auctionReducer,
 }));
